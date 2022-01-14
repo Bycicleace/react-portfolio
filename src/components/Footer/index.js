ed0816839568e7c9c7b5dd3bf2e81f03fad86a05
kvammee <elliott.kvamme@gmail.com>
2022-01-14 17:41:31 -0600

    Added GitHub and LinkedIn icon/buttons to the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,8 +5,16 @@ function Footer() {
         <div className='flex-row space-between'>
             <h3 className='mx-2'>&copy; Elliott Kvamme, 2022</h3>
             <ul className='flex-row mx-2'>
-                <div className='mx-2' id="github">GH</div>
-                <div id="linkedin">LI</div>
+                <div className='mx-2' id="github">
+                    <a href="https://www.github.com/Bycicleace">
+                        <img src="./icons/GH-Light-32x32.png" />
+                    </a>
+                </div>
+                <div id="linkedin">
+                    <a href="https://www.linkedin.com/in/elliottkvamme/">
+                        <img src="./icons/LI-32x32.png" />
+                    </a>
+                </div>
             </ul>
         </div>
     )
