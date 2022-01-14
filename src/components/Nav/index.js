70a9717e8c0f440e3fa93d296faac70d65d5b7e3
kvammee <elliott.kvamme@gmail.com>
2022-01-14 15:41:40 -0600

    Added some colors to CSS

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Nav() {
     return (
-        <header className='flex-row px-1'>
+        <header className='flex-row px-1 space-between'>
             <h1>
                 <a href="/">
                     EK
@@ -10,16 +10,16 @@ function Nav() {
             </h1>
             <nav>
                 <ul className='flex-row'>
-                    <li>
+                    <li className='mx-2'>
                         <a href="#about">About</a>
                     </li>
-                    <li>
+                    <li className='mx-2'>
                         <a href="#projects">Projects</a>
                     </li>
-                    <li>
+                    <li className='mx-2'>
                         <a href="#contact">Contact</a>
                     </li>
-                    <li>
+                    <li className='mx-2'>
                         <a href="#resume">Resume</a>
                     </li>
                 </ul>
