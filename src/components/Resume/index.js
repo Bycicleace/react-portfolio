a4cf12090ddb951c313a69feb6cce30eb90acb5a
kvammee <elliott.kvamme@gmail.com>
2022-01-16 14:13:02 -0600

    Did some formatting

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Resume() {
     return (
-        <div>
-            <h2>Resume</h2>
-            <div id="resume">
-                <section>
+        <div id="resume" class="flex-row flex-wrap flex-start">
+            <h2  class="mx-2">Resume</h2>
+            <div>
+                <section class="mx-2">
                     <h3>Skills</h3>
                     <ul>
                         <li>HTML, CSS, and JavaScript</li>
@@ -18,22 +18,20 @@ function Resume() {
                         <li>Works well independently and on a team</li>
                     </ul>
                 </section>
-                <section>
+                <section class="mx-2">
                     <h3>Education</h3>
-                    <span>B.S. Computer Science, <strong>University of Phoenix</strong>, October 2016</span>
-                    <br />
-                    <span>A.A. General Studies, <strong>Anoka-Ramsey Community College</strong>, September 2010</span>
-                    <br />
-                    <span>High School Diploma, <strong>Cambridge-Isanti High School</strong>, June 2009</span>
+                    <span>B.S. Computer Science, <strong>University of Phoenix</strong>, October 2016</span><br /><br />
+                    <span>A.A. General Studies, <strong>Anoka-Ramsey Community College</strong>, September 2010</span><br /><br />
+                    <span>High School Diploma, <strong>Cambridge-Isanti High School</strong>, June 2009</span><br /><br />
                 </section>
-                <section>
+                <section class="mx-2">
                     <h3>Experience</h3>
-                    <span>Software Developer, <strong>TruStone Financial Credit Union</strong>, December 2017 - Present</span><br />
-                    <span>IT Administrator, <strong>Cross Pointe Church</strong>, January 2018 - November 2021</span><br />
-                    <span>Desktop Support Analyst, <strong>TruStone Financial Credit Union</strong>, August 2016 - December 2017</span><br />
-                    <span>Lead Teller, <strong>TruStone Financial Credit Union</strong>, December 2014 - August 2016</span><br />
-                    <span>Teller, <strong>Minnco Credit Union</strong>, October 2013 - November 2014</span><br />
-                    <span>Assistant Manager, <strong>The Corner Cup</strong>, October 2012 - February 2014</span><br />
+                    <span>Software Developer, <strong>TruStone Financial Credit Union</strong>, December 2017 - Present</span><br /><br />
+                    <span>IT Administrator, <strong>Cross Pointe Church</strong>, January 2018 - November 2021</span><br /><br />
+                    <span>Desktop Support Analyst, <strong>TruStone Financial Credit Union</strong>, August 2016 - December 2017</span><br /><br />
+                    <span>Lead Teller, <strong>TruStone Financial Credit Union</strong>, December 2014 - August 2016</span><br /><br />
+                    <span>Teller, <strong>Minnco Credit Union</strong>, October 2013 - November 2014</span><br /><br />
+                    <span>Assistant Manager, <strong>The Corner Cup</strong>, October 2012 - February 2014</span><br /><br />
                     <span>Customer Service Manager/Cashier/Stocker, <strong>Cub Foods</strong>, November 2007 - February 2013</span>
                 </section>
             </div>
