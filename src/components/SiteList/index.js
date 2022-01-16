370bfb152c25b95b47adb183f6bf8752af4dd7d3
kvammee <elliott.kvamme@gmail.com>
2022-01-16 12:10:40 -0600

    Added styling to projects

--- src/components/SiteList/index.js
+++ src/components/SiteList/index.js
@@ -2,43 +2,43 @@
 import React, { useState } from 'react';
 
 function SiteList() {
-    const [components] = useState([
-        {
-            index: 1,
-            name: "HTML"
-        },
-        {
-            index: 2,
-            name: "CSS"
-        },
-        {
-            index: 3,
-            name: "JavaScript"
-        },
-        {
-            index: 4,
-            name: "JQuery"
-        },
-        {
-            index: 5,
-            name: "Moment.js"
-        },
-        {
-            index: 6,
-            name: "Bootstrap"
-        },
-        {
-            index: 7,
-            name: "API Calls"
-        }
-    ])
+    // const components = [
+    //     {
+    //         index: 1,
+    //         name: "HTML"
+    //     },
+    //     {
+    //         index: 2,
+    //         name: "CSS"
+    //     },
+    //     {
+    //         index: 3,
+    //         name: "JavaScript"
+    //     },
+    //     {
+    //         index: 4,
+    //         name: "JQuery"
+    //     },
+    //     {
+    //         index: 5,
+    //         name: "Moment.js"
+    //     },
+    //     {
+    //         index: 6,
+    //         name: "Bootstrap"
+    //     },
+    //     {
+    //         index: 7,
+    //         name: "API Calls"
+    //     }
+    // ]
 
     const [sites] = useState([
         {
             name: "Run Buddy",
             gitHubURL: "https://github.com/Bycicleace/run-buddy",
             liveSiteURL: "https://bycicleace.github.io/run-buddy/",
-            image: "../../assets/projects/run_buddy.png",
+            image: "run_buddy.png",
             description: "Demonstrates knowledge and use of HTML and CSS",
             componentsUsed: [1, 2],
             bootcamp: true,
@@ -48,17 +48,17 @@ function SiteList() {
             name: "Password Generator",
             gitHubURL: "https://github.com/Bycicleace/password-generator",
             liveSiteURL: "https://bycicleace.github.io/password-generator/",
-            image: "./assets/projects/password_generator.png",
+            image: "password_generator.png",
             description: "Demonstrates knowledge and use of basic JavaScript",
             componentsUsed: [1, 2, 3],
             bootcamp: true,
-            displayIndex: 2
+            displayIndex: 99
         },
         {
             name: "Workday Scheduler",
             gitHubURL: "https://github.com/Bycicleace/WorkdayScheduler",
             liveSiteURL: "https://bycicleace.github.io/WorkdayScheduler/",
-            image: "./assets/projects/workday_scheduler.png",
+            image: "workday_scheduler.png",
             description: "Demonstrates the usage of 3rd party JavaScript libraries like JQuery and Moment.js",
             componentsUsed: [1, 2, 3, 4, 5, 6],
             bootcamp: true,
@@ -68,17 +68,17 @@ function SiteList() {
             name: "Git It Done",
             gitHubURL: "https://github.com/Bycicleace/git-it-done",
             liveSiteURL: "https://bycicleace.github.io/git-it-done/",
-            image: "./assets/projects/git_it_done.png",
+            image: "git_it_done.png",
             description: "Demonstrates the usage of Server Side API calls",
             componentsUsed: [1, 2, 3],
             bootcamp: true,
-            displayIndex: 1
+            displayIndex: 99
         },
         {
             name: "Weather Dashboard",
             gitHubURL: "https://github.com/Bycicleace/weather-dashboard",
             liveSiteURL: "https://bycicleace.github.io/weather-dashboard/",
-            image: "./assets/projects/weather_dashboard.png",
+            image: "weather_dashboard.png",
             description: "Demonstrates the usage of Server Side API calls",
             componentsUsed: [1, 2, 3, 7],
             bootcamp: true,
@@ -92,7 +92,7 @@ function SiteList() {
         //     description: "",
         //     componentsUsed: [],
         //     bootcamp: true,
-        //     displayIndex: -1
+        //     displayIndex: 99
         // }
     ]);
 
@@ -102,16 +102,18 @@ function SiteList() {
     })
 
     return (
-        <div className='flex-row flex-wrap'>
+        <div id="projects" className='flex-row flex-wrap'>
             {sortedSites.map((site) => (
-                <a href={site.liveSiteURL} target='_blank'>
-                    <img
-                        src={site.image}
-                        alt={`${site.name} site`}
-                        className='mx-1'
-                        key={site.name}
-                    />
-                </a>
+                <div className='px-1 py-1'>
+                    <h3>{site.name}</h3>
+                    <a href={site.liveSiteURL} target='_blank' key={site.name}>
+                        <img
+                            src={require(`../../assets/projects/${site.image}`)}
+                            alt={`${site.name} site`}
+                        />
+                    </a>
+                    <p>{site.description}</p>
+                </div>
             ))}
         </div>
     )
